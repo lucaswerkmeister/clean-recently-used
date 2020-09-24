e444a50f9c5079b57ca0960c9c0a41a9619306e2
Lucas Werkmeister <mail@lucaswerkmeister.de>
2020-09-24 22:19:31 +0200

    Initial commit
    Can be used to clean entries below any number of paths from the user’s
    bookmarks to recently used resources. Paths are specified as command
    line arguments. Writes a temporary file while working and atomically
    moves it to the real file at the end.

---
+++ src/main.rs
@@ -0,0 +1,126 @@
+use std::borrow::Cow;
+use std::env;
+use std::error::Error;
+use std::fmt;
+use std::fs::{rename, File, OpenOptions};
+use std::io::BufReader;
+use std::result::Result;
+use std::str;
+use std::vec::Vec;
+use chrono::prelude::*;
+use directories::BaseDirs;
+use itertools::Itertools;
+use quick_xml::{Reader, Writer};
+use quick_xml::events::Event;
+use quick_xml::events::attributes::{Attribute, Attributes};
+
+#[derive(Debug)]
+struct NoBaseDirsError;
+impl fmt::Display for NoBaseDirsError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "NoBaseDirsError")
+    }
+}
+impl Error for NoBaseDirsError {}
+
+#[derive(Debug)]
+struct BookmarkWithoutSingleHrefError;
+impl fmt::Display for BookmarkWithoutSingleHrefError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "BookmarkWithoutSingleHrefError")
+    }
+}
+impl Error for BookmarkWithoutSingleHrefError {}
+
+#[derive(Debug)]
+struct HrefNotFileError;
+impl fmt::Display for HrefNotFileError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "HrefNotFileError")
+    }
+}
+impl Error for HrefNotFileError {}
+
+fn href_attribute(attributes: Attributes) -> Result<Cow<'_, [u8]>, BookmarkWithoutSingleHrefError> {
+    attributes.filter_map(|a| {
+        match a {
+            Ok(Attribute { key: b"href", value }) => Some(value),
+            _ => None,
+        }
+    }).exactly_one().map_err(|_e| BookmarkWithoutSingleHrefError)
+}
+
+fn path_needs_cleaning(paths_to_clean: &Vec<String>, path: &str) -> bool {
+    paths_to_clean.iter().any(|path_to_clean| path.starts_with(path_to_clean))
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let base_dirs = BaseDirs::new().ok_or(NoBaseDirsError)?;
+    let dir = base_dirs.data_dir();
+    let input_filename = dir.join("recently-used.xbel");
+    let output_filename = dir.join(Local::now().format("recently-used.xbel-%+").to_string());
+    let paths_to_clean: Vec<String> = env::args().skip(1).collect();
+
+    let input_file = File::open(&input_filename)?;
+    let reader = BufReader::new(input_file);
+    let mut reader = Reader::from_reader(reader);
+    let mut buf = Vec::new();
+
+    let output_file = OpenOptions::new().write(true).create_new(true).open(&output_filename)?;
+    let mut writer = Writer::new(output_file);
+
+    let mut skipping = false;
+    let mut skip_whitespace = false;
+
+    loop {
+        if skipping {
+            match reader.read_event(&mut buf) {
+                Ok(Event::End(e)) if e.name() == b"bookmark" => {
+                    skipping = false;
+                    skip_whitespace = true;
+                },
+                _ => (),
+            }
+        } else {
+            match reader.read_event(&mut buf) {
+                Ok(Event::Start(e)) => {
+                    if e.name() == b"bookmark" {
+                        let attr = href_attribute(e.attributes())?;
+                        let href = str::from_utf8(&attr)?;
+                        let path = href.strip_prefix("file://").ok_or(HrefNotFileError)?;
+                        // TODO this file may be percent-encoded
+                        if path_needs_cleaning(&paths_to_clean, path) {
+                            skipping = true;
+                            continue;
+                        }
+                    }
+                    writer.write_event(Event::Start(e))?;
+                },
+                Ok(Event::End(e)) => {
+                    writer.write_event(Event::End(e))?;
+                },
+                Ok(Event::Empty(e)) => {
+                    writer.write_event(Event::Empty(e))?;
+                }
+                Ok(Event::Text(e)) => {
+                    if skip_whitespace {
+                        skip_whitespace = false;
+                        assert!(e.unescape_and_decode(&reader)?.chars().all(char::is_whitespace));
+                    } else {
+                        writer.write_event(Event::Text(e))?;
+                    }
+                },
+                Ok(Event::Eof) => break,
+                Ok(Event::Decl(e)) => {
+                    writer.write_event(Event::Decl(e))?;
+                },
+                Err(e) => panic!("Error at position {}: {:?}", reader.buffer_position(), e),
+                other => unimplemented!("{:?}", other),
+            }
+        }
+    }
+
+    rename(output_filename, input_filename)?;
+
+    Ok(())
+}
